f5acad42b64267b1f157f56654ee2d89fad4a741
William Stamp <100212618+WILLIAMSTAMP@users.noreply.github.com>
2022-10-04 21:58:11 -0230

    Update logEvents.js

--- logEvents.js
+++ logEvents.js
@@ -7,19 +7,15 @@ const { v4: uuid } = require('uuid');
 const fs = require('fs');
 const fsPromises = require('fs').promises;
 const path = require('path');
-
+// Data to be logged
 const logEvents = async (event, level, message, statusCode) => {
     const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
     const logItem = `${dateTime}\t${level}\t${statusCode}\t${event}\t${message}\t${uuid()}`;
-
     try {
-        // TODO: include year and month when managing folders
         if(!fs.existsSync(path.join(__dirname, 'logs'))) {
-            // include ./logs/yyyy/mmmm
             await fsPromises.mkdir(path.join(__dirname, 'logs'));
         }
-        // Include todays date in filename
-        const fileName = `${format(new Date(), 'yyyyMMdd')}` + '_httpevents.log';
+        const fileName = `${format(new Date(), 'yyyyMMdd')}` + '_events.log';
         await fsPromises.appendFile(path.join(__dirname, 'logs', fileName), logItem + '\n');
     } catch (err) {
         console.log(err);
